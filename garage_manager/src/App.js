348e63388253bf584af04566f935c35909348f9b
brema026 <Nif_Zawtx@outlook.com>
2026-01-04 17:03:19 -0600

    Estructura Inicial

---
+++ garage_manager/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Users from "./pages/Users";
+import Teams from "./pages/Teams";
+import Drivers from "./pages/Drivers";
+import Sponsors from "./pages/Sponsors";
+import Parts from "./pages/Parts";
+import Inventory from "./pages/Inventory";
+import CarSetup from "./pages/CarSetup";
+
+function App() {
+  const [view, setView] = useState("users");
+
+  const renderView = () => {
+    switch (view) {
+      case "users": return <Users />;
+      case "teams": return <Teams />;
+      case "drivers": return <Drivers />;
+      case "sponsors": return <Sponsors />;
+      case "parts": return <Parts />;
+      case "inventory": return <Inventory />;
+      case "setup": return <CarSetup />;
+      default: return <Users />;
+    }
+  };
+
+  return (
+    <div>
+      <Navbar setView={setView} />
+      <div className="container">
+        {renderView()}
+      </div>
+    </div>
+  );
+}
+
+export default App;
