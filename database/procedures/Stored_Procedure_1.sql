fcd9528521b45787bfded423a8fe4a4c7fe4baec
brema026 <Nif_Zawtx@outlook.com>
2026-01-18 17:30:02 -0600

    cambio necesario en sp de aportes

--- database/procedures/Stored_Procedure_1.sql
+++ database/procedures/Stored_Procedure_1.sql
@@ -9,8 +9,7 @@ BEGIN
         e.nombre,
         ISNULL(SUM(a.monto), 0) AS presupuesto_total
     FROM dbo.equipo e
-    LEFT JOIN dbo.aporte a
-        ON a.id_equipo = e.id_equipo
+    LEFT JOIN dbo.aporte a ON a.id_equipo = e.id_equipo
     WHERE e.id_equipo = @id_equipo
     GROUP BY e.id_equipo, e.nombre;
 END;
